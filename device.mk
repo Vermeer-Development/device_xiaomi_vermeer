ByteWave1014/tree-w

vermeer: Initial Rebrand Signed-off-by: Lunark :3 <jvillaltamatos3007@gmail.com>
Change-Id: I3779571b4819c2fe47616721f29721fe7f09e9b6

device.mk
@@ -174,7 +174,7 @@ PRODUCT_PACKAGES += \
 
 # Init
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/init/init.socrates.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.socrates.rc \
+    $(LOCAL_PATH)/init/init.xiaomi_sm8550.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.xiaomi_sm8550.rc \
     $(LOCAL_PATH)/init/init.recovery.qcom.rc:recovery/root/init.recovery.qcom.rc
 
 # IPA
@@ -237,11 +237,11 @@ PRODUCT_COPY_FILES += \
 
 # Overlay
 PRODUCT_PACKAGES += \
-    CarrierConfigOverlaySocrates \
-    FrameworkResOverlaySocrates \
-    SystemUIOverlaySocrates \
-    SettingsOverlaySocrates \
-    TelephonyOverlaySocrates
+    CarrierConfigOverlayVermeer \
+    FrameworkResOverlayVermeer \
+    SystemUIOverlayVermeer \
+    SettingsOverlayVermeer \
+    TelephonyOverlayVermeer
 
 # Power
 PRODUCT_PACKAGES += \
@@ -346,7 +346,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
 # Vendor configurations
-$(call inherit-product, vendor/xiaomi/socrates/socrates-vendor.mk)
+$(call inherit-product, vendor/xiaomi/vermeer/vermeer-vendor.mk)
 
 # Vendor service manager
 PRODUCT_PACKAGES += \

Vermeer-Development/device_xiaomi_vermeer

device.mk
@@ -174,7 +174,7 @@ PRODUCT_PACKAGES += \
 
 # Init
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/init/init.socrates.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.socrates.rc \
+    $(LOCAL_PATH)/init/init.vermeer.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.vermeer.rc \
     $(LOCAL_PATH)/init/init.recovery.qcom.rc:recovery/root/init.recovery.qcom.rc
 
 # IPA
@@ -237,11 +237,11 @@ PRODUCT_COPY_FILES += \
 
 # Overlay
 PRODUCT_PACKAGES += \
-    CarrierConfigOverlaySocrates \
-    FrameworkResOverlaySocrates \
-    SystemUIOverlaySocrates \
-    SettingsOverlaySocrates \
-    TelephonyOverlaySocrates
+    CarrierConfigOverlayVermeer \
+    FrameworkResOverlayVermeer \
+    SystemUIOverlayVermeer \
+    SettingsOverlayVermeer \
+    TelephonyOverlayVermeer
 
 # Power
 PRODUCT_PACKAGES += \
@@ -346,7 +346,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
 # Vendor configurations
-$(call inherit-product, vendor/xiaomi/socrates/socrates-vendor.mk)
+$(call inherit-product, vendor/xiaomi/vermeer/vermeer-vendor.mk)
 
 # Vendor service manager
 PRODUCT_PACKAGES += \